2fe7c0b2a825ded619be32e0e2101b755efe808c
Hannele Ruiz <justlemoncl@gmail.com>
2023-10-26 01:04:34 -0300

    Added function to get the synapse executable path

--- SynapseRestarter/Program.cs
+++ SynapseRestarter/Program.cs
@@ -1,12 +1,43 @@
-﻿namespace SynapseRestarter;
+﻿using System.IO;
+using Microsoft.Win32;
 
-using System;
+namespace SynapseRestarter;
 
 /// <summary>
 /// The core of SynapseRestarter.
 /// </summary>
 public static class Program
 {
+    /// <summary>
+    /// Gets the executable of Razer Synapse 3.
+    /// </summary>
+    /// <returns>The path to the main Razer Synapse 3 executable.</returns>
+    public static string? GetExecutable()
+    {
+        RegistryKey? key = Registry.LocalMachine.OpenSubKey("SOFTWARE\\WOW6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Razer Synapse");
+
+        if (key == null)
+        {
+            return null;
+        }
+
+        object? icon = key.GetValue("DisplayIcon");
+
+        if (icon is not string iconFilePath)
+        {
+            return null;
+        }
+
+        string? synapsePath = Path.GetDirectoryName(Path.GetDirectoryName(iconFilePath));
+
+        if (synapsePath == null)
+        {
+            return null;
+        }
+
+        return Path.Join(synapsePath, "WPFUI", "Framework", "Razer Synapse 3 Host", "Razer Synapse 3.exe");
+    }
+    
     /// <summary>
     /// The main entry point.
     /// </summary>
