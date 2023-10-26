500ea2d6bf0e8d61c24ce6b512ba9f8224773e7d
Hannele Ruiz <justlemoncl@gmail.com>
2023-10-26 00:09:23 -0300

    Added basic program class

---
+++ SynapseRestarter/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace SynapseRestarter;
+
+using System;
+
+/// <summary>
+/// The core of SynapseRestarter.
+/// </summary>
+public static class Program
+{
+    /// <summary>
+    /// The main entry point.
+    /// </summary>
+    public static void Main()
+    {
+    }
+}
